668ce31bdf6bc4d8aaadcefbdd276d168ab2c1fe
unknown <nevenaradesic@gmail.com>
2023-10-24 23:22:03 +0200

    dodati fajl za povezivanje sa robotom i konverziju govora u tekst

---
+++ backend/sphero-nlp/sphero-skd.py
@@ -0,0 +1,17 @@
+import time
+from spherov2 import scanner
+from spherov2.sphero_edu import SpheroEduAPI
+from spherov2.types import Color
+
+toy = scanner.find_toy()
+
+with SpheroEduAPI(toy) as droid:
+
+    droid.set_main_led(Color(r=255, g=0, b=0))
+    droid.set_heading(0)
+    droid.set_speed(30)
+
+    droid.roll(150, 30, 3)
+
+    time.sleep(3000)
+    droid.roll(0, 0, 1)
